990eaec99b1f27faf4df16c91d3de196c6b7bcc1
Hubcapp <walnut.alligator@gmail.com>
2019-04-15 12:16:47 -0500

    Copy Logger from RSC+ and add logo. Create Settings file.

---
+++ src/rscminus/common/Settings.java
@@ -0,0 +1,67 @@
+/**
+ * rscminus
+ *
+ * This file is part of rscminus.
+ *
+ * rscminus is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * rscminus is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with rscminus. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: see <https://github.com/OrN/rscminus>
+ */
+
+package rscminus.common;
+
+
+import java.net.URL;
+
+public class Settings {
+  public static int LOG_VERBOSITY = 0;
+  public static boolean COLORIZE_CONSOLE_TEXT = true;
+  public static boolean LOG_FORCE_LEVEL = true;
+  public static boolean LOG_SHOW_LEVEL = true;
+  public static boolean LOG_FORCE_TIMESTAMPS = true;
+  public static boolean LOG_SHOW_TIMESTAMPS = true;
+
+  public static class Dir {
+
+    public static String JAR;
+    public static String DUMP;
+    public static String SCREENSHOT;
+    public static String REPLAY;
+  }
+
+  public static void initDir() {
+    Dir.JAR = ".";
+    try {
+      Dir.JAR =
+              Settings.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+      int indexFileSep1 = Dir.JAR.lastIndexOf('/');
+      int indexFileSep2 = Dir.JAR.lastIndexOf('\\');
+      int index = (indexFileSep1 > indexFileSep2) ? indexFileSep1 : indexFileSep2;
+      if (index != -1) Dir.JAR = Dir.JAR.substring(0, index);
+    } catch (Exception e) {
+    }
+  }
+
+  public static URL getResource(String fileName) { // TODO: Consider moving to a more relevant place
+    URL url = null;
+
+    // Try finding assets
+    try {
+      url = new URL("file://" + Dir.JAR + fileName);
+    } catch (Exception e) {
+    }
+    Logger.Debug("Dir.JAR: " + Dir.JAR);
+    Logger.Info("Loading resource: " + fileName);
+
+    return url;
+  }
+}
